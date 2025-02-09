9975044a63056e69e511bbcaf0376cbeea37d5a6
ivanhez <64239629+ivanhez@users.noreply.github.com>
2025-02-09 10:33:25 -0600

    Update LAB3_BD2.py

--- LAB3_BD2.py
+++ LAB3_BD2.py
@@ -1,68 +1,36 @@
+import random
+from datetime import date, timedelta
 from neo4j import GraphDatabase
 
-# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+# ------------------------------------------------------------------------------
+# Configuración
+# ------------------------------------------------------------------------------
 URI = "neo4j+s://5e7134fe.databases.neo4j.io"
 AUTH = ("neo4j", "vg4OqlW3WQ5fs5_lDt2xKgHTz_09qix4HLGqzOrwEpg")
 
-employee_threshold=10
+# ------------------------------------------------------------------------------
+# Funciones para Personas
+# ------------------------------------------------------------------------------
+NUM_PEOPLE = 20
+def random_date(start_year=1950, end_year=2000):
+    start_date = date(start_year, 1, 1)
+    end_date   = date(end_year, 12, 31)
+    delta_days = (end_date - start_date).days
+    random_days = random.randrange(delta_days)
+    return start_date + timedelta(days=random_days)
 
-
-def main():
-    with GraphDatabase.driver(URI, auth=AUTH) as driver:
-        with driver.session(database="neo4j") as session:
-            for i in range(100):
-                name = f"Thor{i}"
-                org_id = session.execute_write(employ_person_tx, name)
-                print(f"User {name} added to organization {org_id}")
-            
-
-            # movie = session.execute_read(find_movie_tx, "The Matrix")
-            # print(movie)
-
-
-def employ_person_tx(tx, name):
-    # Create new Person node with given name, if not exists already
-    result = tx.run("""
-        MERGE (p:Person {name: $name})
-        RETURN p.name AS name
-        """, name=name
-    )
-
-    # Obtain most recent organization ID and the number of people linked to it
-    result = tx.run("""
-        MATCH (o:Organization)
-        RETURN o.id AS id, COUNT{(p:Person)-[r:WORKS_FOR]->(o)} AS employees_n
-        ORDER BY o.created_date DESC
-        LIMIT 1
-    """)
-    org = result.single()
-
-    if org is not None and org["employees_n"] == 0:
-        raise Exception("Most recent organization is empty.")
-        # Transaction will roll back -> not even Person is created!
-
-    # If org does not have too many employees, add this Person to that
-    if org is not None and org.get("employees_n") < employee_threshold:
-        result = tx.run("""
-            MATCH (o:Organization {id: $org_id})
-            MATCH (p:Person {name: $name})
-            MERGE (p)-[r:WORKS_FOR]->(o)
-            RETURN $org_id AS id
-            """, org_id=org["id"], name=name
-        )
-
-    # Otherwise, create a new Organization and link Person to it
+def random_label_set():
+    choice = random.choice(["Actor", "Director", "Both"])
+    if choice == "Both":
+        return ["Actor", "Director"]
     else:
-        result = tx.run("""
-            MATCH (p:Person {name: $name})
-            CREATE (o:Organization {id: randomuuid(), created_date: datetime()})
-            MERGE (p)-[r:WORKS_FOR]->(o)
-            RETURN o.id AS id
-            """, name=name
-        )
+        return [choice]
 
-    # Return the Organization ID to which the new Person ends up in
-    return result.single()["id"]
+# ------------------------------------------------------------------------------
+# Función de crear nodo Person
+# ------------------------------------------------------------------------------
+def create_person_tx(tx, name, tmdbId, born, died, bornIn, url, imdbId, bio, poster, labels):
+    labels_str = ":Person" + "".join(f":{label}" for label in labels)
 
 def create_movie_tx(tx, movie_data):
     query = """
